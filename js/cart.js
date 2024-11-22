5986e0f615dc6d1ae03b2068cfddafcaef9a5d02
mateigabriela <mateigabriela202@gmail.com>
2024-11-22 17:59:00 +0000

    feat: cart page css

--- js/cart.js
+++ js/cart.js
@@ -6,12 +6,16 @@ function saveCartProducts(cart) {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
+function calculateCartTotal(cart) {
+    return cart.reduce((total, product) => total + product.price * product.quantity, 0);
+}
+
 function displayCart() {
     const cart = getCartProducts();
     const cartContainer = document.querySelector('.cart-container');
 
     if (!cart.length) {
-        cartContainer.innerHTML = '<p>Coșul este gol!</p>';
+        cartContainer.innerHTML = '<p class="msg-empty-cart">CART IS EMPTY!</p>';
         return;
     }
 
@@ -20,8 +24,8 @@ function displayCart() {
             <img src="${product.imageURL}" alt="${product.name}" class="cart-item-image">
             <div class="cart-item-details">
                 <h3>${product.name}</h3>
-                <p>Price: $${product.price}</p>
-                <p>Total: $${(product.price * product.quantity).toFixed(2)}</p>
+                <p>Price: $${Number(product.price).toFixed(2).endsWith(".00") ? Math.round(product.price) : product.price}</p>
+                <p>Total: $${(product.price * product.quantity).toFixed(2).endsWith(".00") ? Math.round(product.price * product.quantity) : (product.price * product.quantity).toFixed(2)}</p>
                 <label>
                     Quantity:
                     <input type="number" class="quantity-input" data-product-id="${product.id}" value="${product.quantity}" min="1">
@@ -30,6 +34,14 @@ function displayCart() {
             </div>
         </div>
     `).join('');
+    
+
+    const total = calculateCartTotal(cart);
+    cartContainer.innerHTML += `
+        <div class="cart-total">
+            <h2>Total Cart Value: $${total.toFixed(2)}</h2>
+        </div>
+    `;
 
     document.querySelectorAll('.delete-button').forEach(button => {
         button.addEventListener('click', (e) => deleteProductFromCart(e.target.dataset.productId));
@@ -44,7 +56,7 @@ function deleteProductFromCart(productId) {
     let cart = getCartProducts();
     cart = cart.filter(product => product.id !== productId);
     saveCartProducts(cart);
-    displayCart();
+    displayCart(); 
 }
 
 function updateProductQuantity(productId, quantity) {
@@ -53,7 +65,7 @@ function updateProductQuantity(productId, quantity) {
     if (product) {
         product.quantity = parseInt(quantity, 10) || 1; 
         saveCartProducts(cart);
-        displayCart();
+        displayCart(); 
     }
 }
 
